9150f581c3ba6b3e690d70b85ce658ad985540b4
victor-m-p <victormoeller@gmail.com>
2022-11-18 14:59:13 -0500

    fixing class and running more stuff

--- DRH/data_selection.py
+++ DRH/data_selection.py
@@ -2,138 +2,182 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-from helper_functions import *
+import itertools
 
 # read data
 df = pd.read_csv("data/raw/df_raw.csv")
+df = df[["entry_id", "entry_name", "related_q_id", "related_q", "related_parent_q", "answers"]] 
 
-##### BASIC PREPROCESSING #####
-## subset relevant columns
-df = df[["related_q", "related_q_id", "answers", "answer_val", "related_parent_q", "entry_name", "entry_id"]]
-## only overall questions (without parent)
-df = df[df["related_parent_q"].isna()]
-## if more than one answer to a question sample only 1 answer. 
-## maybe this should be later actually 
-df = df.sample(frac = 1.0).groupby(['related_q_id', 'entry_id']).head(1)
-## check for yes/no questions 
-conditions = [
-    (df['answers'] == "Yes"),
-    (df["answers"] == "No"),
-    (df['answers'] == "Field doesn't know") | (df["answers"] == "I don't know") | (df['answers'] == "NaN")
-]
-choices = ["Yes", "No", "DK-NAN"]
-df['answer_types'] = np.select(conditions, choices, default="non-binary")
+# for now outside the function
+def fill_grid(df, c1, c2, fill):
 
-df.groupby('answer_types').size()
+    l_c1 = df[c1].drop_duplicates().to_list()
+    l_c2 = df[c2].drop_duplicates().to_list()
 
-## find questions with non-binary answers and remove them
-df_ = df.groupby(['related_q_id', 'answer_types']).size().reset_index(name="count")
-df_ = df_[df_["answer_types"] == "non-binary"]
-df_ = df.merge(df_, how = "outer", indicator = True)
-df_b = df_[(df_._merge=="left_only")].drop("_merge", axis = 1)
+    l_comb = list(itertools.product(l_c1, l_c2))
+    d_comb = pd.DataFrame(l_comb, columns=[c1, c2])
 
-## fill with nan 
-df_nan = fill_grid(df_b, "related_q_id", "entry_id", "DK-NAN")
+    df = df.merge(d_comb, on = [c1, c2], how = "outer").fillna(fill)
+    return df 
 
-## recode values
-answer_map = {
-    "DK-NAN": 0,
-    "Yes": 1,
-    "No": -1
-}
-df_nan = df_nan.assign(answers = df_nan["answer_types"].map(answer_map))
-df_nan = df_nan[["entry_id", "related_q_id", "answers"]]
+# sort-of works as intended now 
+civ = Civilizations(df)
+civ.preprocess() # can only be run once (fixed)
+civ.set_constraints(20, 7/20, "related_q_id") # when we run this
+civ.n_best()
+civ.max_constraints()
+civ.max_tolerance()
+civ.create_matrix() # (180, 20)
+#civ.status()
+civ.save_dat("data/clean/")
 
-##### END BASIC PREPROCESSING ######
+pd.set_option('display.max_colwidth', None)
+
+nref = pd.read_csv("data/clean/n_20_tol_0_nref.csv")
+sref = pd.read_csv("data/clean/n_20_tol_0_sref.csv")
+d = pd.read_csv("data/clean/n_20_tol_0.csv")
+A = np.loadtxt("data/clean/n_20_tol_0.txt") 
+d[["related_q_id"]].drop_duplicates() # 
+
+# try with tolerance
+civ = Civilizations(df)
 
 ##### CONDITION #####
-## only questions that are not 100% one thing
-class civs:
-    def __init__(self, d, n, s, a, m):
-        self.d = d # basic data
-        self.n = n # questions column (nodes) - related_q_id
-        self.s = s # samples column (civilization) - entry_id
-        self.a = a # answer column (e.g. Yes = 1, No = -1, DK/NAN = 0) - answers
-        self.m = m # value to minimize (unknown, nan) - 0
-
-    def sort_n(self):
-        d_n = self.d.groupby([self.n, self.a]).size().reset_index(name="count")
-        d_n = d_n[d_n[self.a] == self.m].sort_values("count", ascending=True)
-        self.d_n = d_n 
-
-    def sort_s(self): 
-        d_s = self.d.groupby([self.s, self.a]).size().reset_index(name="count")
-        d_s = d_s[d_s[self.a] == self.m].sort_values("count", ascending=True)
-        self.d_s = d_s
+## todo: only questions that are not 100% one thing
+class Civilizations:
     
-    def n_best(self, tol, nq):
-        # get the best N nodes (questions)
-        d_ = self.d_n[[self.n]].head(nq) # len n_q
-        # inner join with data
-        d_top = self.d.merge(d_, on = self.n, how = 'inner')
-        # samples answer distribution for best N nodes 
-        d_ = d_top.groupby([self.s, self.a]).size().reset_index(name = 'count')
-        # fill grid if incomplete 
-        d_ = fill_grid(d_, self.s, self.a, self.m) # just fill zeros
-        # sort values by answer to minimize (DK/NAN) 
-        d_ = d_[d_[self.a] == self.m].sort_values('count', ascending = True)
-        # assign fraction for when we have tolerance > 0
-        d_['frac'] = d_['count']/nq
-        # take all samples with lower DK/NAN than tolerance
-        d_ = d_[d_['frac'] <= tol]
-        d_ = d_top.merge(d_[[self.s]].drop_duplicates(), on = self.s, how = "inner")
-        d_.sort_values([self.s, self.n], ascending = [True, True], inplace = True)
-        self.d_n_t = d_
+    def __init__(self, d, m=0, tol=0, ntot=0):
+        self.d = d
+        self.minv = m # value to minimize (unknown, nan) - 0 (should change...)
+        self.tol = tol
+        self.ntot = ntot 
+        self.clean = False 
+        self.sortc = False 
+        self.optimc = False
+        self.order_c()
     
-    def s_best(self, tol, nc):
-        # get best N samples (civs)
-        dx = self.d_s[[self.s]].head(nc) # 200
-        # inner join with data
-        dx_top = self.d.merge(dx, on = self.s, how = 'inner')
-        # 
-        dx = dx_top.groupby([self.n, self.a]).size().reset_index(name = 'count')
-        # fill grid (just zeros filled, so not complete)
-        dx = fill_grid(dx, self.n, self.a, self.m)
-        #
-        dx = dx[dx[self.a] == self.m].sort_values('count', ascending = True)
-        # assign frac 
-        dx['frac'] = dx['count']/nc
-        # take values out 
-        dx = dx[dx['frac'] <= tol] # n = 46
-        dx = dx_top.merge(dx[[self.n]].drop_duplicates(), on = self.n, how = "inner")
-        dx.sort_values([self.s, self.n], ascending = [True, True], inplace = True)
-        self.d_s_t = dx
+    # create variables based on column order
+    def order_c(self):
+        self.sc, self.sc_, self.nc, self.nc_, self.pc, self.ac = self.d.columns
+        self.check_cols()
+        self.status()
+         
+    def check_cols(self):
+        print(f"-COLUMNS-")
+        print(f"samples id: {self.sc}")
+        print(f"samples name: {self.sc_}")
+        print(f"nodes id: {self.nc}")
+        print(f"nodes name: {self.nc_}")
+        print(f"parent: {self.pc}")
+        print(f"answers: {self.ac}")
     
-    def create_mat(self, tol, nq):
-        self.sort_n()
-        self.n_best(tol, nq)
-        d_pivot = self.d_n_t.pivot(
-            index = self.s,
-            columns = self.n,
-            values = self.a)
-        self.A = np.array(d_pivot)
-    
-    def save_dat(self, path, tol, nq):
-        # need somehow to test whether this already happened
-        # i.e. can we check whether it exists?
-        self.create_mat(tol, nq)
+    # check status of the pipeline 
+    def status(self):
+        print(f'-STATUS-')
+        print(f'd original (nodes, samples): ({len(self.d[self.nc].unique())}, {len(self.d[self.sc].unique())})')
+        if self.clean: 
+            print(f'd preprocessed (nodes, samples): ({len(self.dmain[self.nc].unique())}, {len(self.dmain[self.sc].unique())})')
+        else: 
+            print(f'd preprocessed: {self.clean}')
+        print(f'tolerance: {self.tol}')
+        print(f'total: {self.ntot}')
+        print(f'sorting column: {self.sortc}')    
+        print(f'optimize column: {self.optimc}')
         
-        #x = self.d.merge(self.d_n_t, on = "related_q_id", how = "inner")
-        #x = x[["related_q_id", "related_q"]].drop_duplicates()
-        # save stuff
-        x.to_csv(f"{path}n_20_tol_0_q.csv", index = False)
-        np.savetxt(f"{path}n_20_tol_0.txt", self.A.astype(int), fmt="%i")
-        self.d_n_t.to_csv(f"{path}n_20_tol_0.csv", index = False)
+    # only binary questions (used in 'preprocess' function)
+    def binary(self):
+        # create non-binary
+        conditions = [
+            (self.dmain[self.ac] == "Yes"),
+            (self.dmain[self.ac] == "No"),
+            (self.dmain[self.ac] == "Field doesn't know") 
+            | (self.dmain[self.ac] == "I don't know") 
+            | (self.dmain[self.ac] == "NaN")
+        ]
+        choices = [1, -1, 0]
+        self.dmain[self.ac] = np.select(conditions, choices, default=100)
 
-civdat = civs(df_nan, "related_q_id", "entry_id", "answers", 0)
-civdat.save_dat("data/clean/", 0, 20)
+        # remove non-binary
+        d_ = self.dmain[self.dmain[self.ac] == 100][[self.nc]].drop_duplicates()
+        print(f"len non-binary: {len(d_)}")
+        self.dmain = self.dmain.merge(d_, on = self.nc, how = "outer", indicator = True)
+        self.dmain = self.dmain[(self.dmain._merge=="left_only")].drop("_merge", axis = 1)
+        
+    # preprocess function
+    def preprocess(self): 
+        if not self.clean:
+            self.dmain = self.d[self.d[self.pc].isna()] # only top-lvl
+            self.dmain = self.dmain[[self.nc, self.sc, self.ac]]
+            self.dmain = self.dmain.sample(frac = 1.0, random_state=142).groupby([self.nc, self.sc]).head(1) # only one answ per q
+            self.binary() # only binary answers
+            self.dmain = fill_grid(self.dmain, self.sc, self.nc, 0) # fill grid with 0 
+            self.dmain[self.ac] = [int(x) for x in self.dmain[self.ac]] # make better 
+            self.clean = True    
+        self.status()
 
-# load it (looks good)
-d = pd.read_csv("data/clean/n_20_tol_0.csv")
-A = np.loadtxt("data/clean/n_20_tol_0.txt", dtype = "int")
-x = d.merge(df, on = "related_q_id", how = "inner")[["related_q_id", "related_q"]].drop_duplicates()
-x.to_csv("data/clean/n_20_tol_0_q.csv", index = False)
+    # right now sorts based on number of 
+    def sort_vals(self): # c = specifying whether it is n (questions) or s (samples) 
+        self.dsort = self.dmain.groupby([self.sortc, self.ac]).size().reset_index(name="count")
+        self.dsort = self.dsort[self.dsort[self.ac] == self.minv].sort_values("count", ascending=True)  
+        
+    # set constraints  
+    def set_constraints(self, ntot, tol, sortc):
+        self.ntot = ntot 
+        self.tol = tol 
+        self.sortc = sortc
+        if self.sortc == self.ac: 
+            self.optimc = self.nc 
+        elif self.sortc == self.nc: 
+            self.optimc = self.sc
+        else: 
+            "Something went wrong here..."
+        self.status() # but probably do not want the other info here...
+    
+    # this should probably just run sort_vals as well...
+    def n_best(self): # c = self.nc
+        self.sort_vals()
+        self.dbest = self.dsort[[self.sortc]].head(self.ntot)
+        self.dbest = self.dmain.merge(self.dbest, on = self.sortc, how = 'inner') # important
+        
+    # we need this to automatically be the opposite of the sorting column
+    def max_constraints(self): # c = self.sc (the opposite one..)
+        self.dgroup = self.dbest.groupby([self.optimc, self.ac]).size().reset_index(name = 'count')
+        self.dgroup = fill_grid(self.dgroup, self.optimc, self.ac, self.minv) # ...?
+        self.dgroup = self.dgroup[self.dgroup[self.ac] == self.minv].sort_values('count', ascending = True)
+        self.dgroup['frac'] = self.dgroup['count']/self.ntot 
+        
+    def max_tolerance(self):
+        #print(f"tolerance: {self.tol}")
+        self.dtol = self.dgroup[self.dgroup['frac'] <= self.tol] # here?
+        self.dmax = self.dbest.merge(self.dtol[[self.optimc]].drop_duplicates(), on = self.optimc, how = 'inner')
+        #self.dmax.sort_values([self.sc, self.nc], ascending = [True, True], inplace = True)
+        
+    # create matrix 
+    def create_matrix(self):
+        self.dmax.sort_values([self.sc, self.nc], ascending = [True, True], inplace = True)
+        d_pivot = self.dmax.pivot(
+            index = self.sc, # s always index 
+            columns = self.nc, # a always columns
+            values = self.ac)
+        self.A = np.array(d_pivot)
+        print(f"A shape: {self.A.shape}")
+    
+    def save_dat(self, path):
+        d_nobs = self.dmax[[self.nc]].drop_duplicates() # unique n
+        d_sobs = self.dmax[[self.sc]].drop_duplicates() # unique s 
+        d_nref = self.d[[self.nc, self.nc_]].drop_duplicates()
+        d_sref = self.d[[self.sc, self.sc_]].drop_duplicates()
+        self.n_ref = d_nobs.merge(d_nref, on = self.nc, how = 'inner').sort_values(self.nc, ascending = True)
+        self.s_ref = d_sobs.merge(d_sref, on = self.sc, how = 'inner').sort_values(self.sc, ascending = True)
+        # print information
+        print(f"maximum X for {self.ntot} best X with tolerance {self.tol}")
+        # save numpy
+        print(f"saving array to '{path}n_{self.ntot}_tol_{self.tol}.txt'")
+        np.savetxt(f"{path}n_{self.ntot}_tol_{self.tol}.txt", self.A.astype(int), fmt="%i")
+        # save csv overall 
+        print(f"saving csv to '{path}_n_{self.ntot}_tol_{self.tol}.csv'")
+        self.dmax.to_csv(f"{path}n_{self.ntot}_tol_{self.tol}.csv", index = False)
+        ## save the other ones 
+        self.n_ref.to_csv(f"{path}n_{self.ntot}_tol_{self.tol}_nref.csv", index = False)
+        self.s_ref.to_csv(f"{path}n_{self.ntot}_tol_{self.tol}_sref.csv", index = False)
 
-# clean this more and make the class better
-# test everything a lot ...
-# make sure that Simon has the right stuff
---
+++ DRH/trash.py
@@ -0,0 +1,220 @@
+
+
+
+######## old stuf #######
+
+# clean this more and make the class better
+# test everything a lot ...
+# make sure that Simon has the right stuff
+# make sure that all questions different kinds of answers
+class civs:
+
+    def __init__(self, d, n, s, a, m):
+        self.d = d # basic data
+        self.n = n # questions column (nodes) - related_q_id
+        self.s = s # samples column (civilization) - entry_id
+        self.a = a # answer column (e.g. Yes = 1, No = -1, DK/NAN = 0) - answers
+        self.m = m # value to minimize (unknown, nan) - 0
+
+    # some duplicate nature here (sort_n, sort_s)
+    def sort_n(self):
+        print(f"-1. len d: {len(self.d)}")
+        d_n = self.d.groupby([self.n, self.a]).size().reset_index(name="count")
+        print(f"0. len d_s: {len(d_n)}")
+        d_n = d_n[d_n[self.a] == self.m].sort_values("count", ascending=True)
+        self.d_n = d_n 
+        print(f"1. len d_s: {len(d_n)}")
+
+    def sort_s(self): 
+        d_s = self.d.groupby([self.s, self.a]).size().reset_index(name="count")
+        d_s = d_s[d_s[self.a] == self.m].sort_values("count", ascending=True)
+        self.d_s = d_s
+    
+    # ... should only be one of the ... # 
+    
+    # some duplicate nature here (n_best, s_best)
+    def n_best(self, tol, nq):
+        d_ = self.d_n[[self.n]].head(nq)
+        print(f"2. len d_: {len(d_)}")
+        d_top = self.d.merge(d_, on = self.n, how = 'inner')
+        print(f"3. len d_top: {len(d_top)}")
+        d_ = d_top.groupby([self.s, self.a]).size().reset_index(name = 'count')
+        print(f"4. len d_: {len(d_)}")
+        d_ = fill_grid(d_, self.s, self.a, self.m)
+        print(f"5. len d_: {len(d_)}")
+        d_ = d_[d_[self.a] == self.m].sort_values('count', ascending = True)
+        print(f"6. len d_: {len(d_)}")
+        d_['frac'] = d_['count']/nq
+        print(f"7. len d_: {len(d_)}")
+        d_ = d_[d_['frac'] <= tol]
+        print(f"8. len d_: {len(d_)}") 
+        d_ = d_top.merge(d_[[self.s]].drop_duplicates(), on = self.s, how = "inner")
+        d_.sort_values([self.s, self.n], ascending = [True, True], inplace = True)
+        self.d_n_t = d_
+
+    def s_best(self, tol, nc):
+        dx = self.d_s[[self.s]].head(nc)
+        dx_top = self.d.merge(dx, on = self.s, how = 'inner')
+        dx = dx_top.groupby([self.n, self.a]).size().reset_index(name = 'count')
+        dx = fill_grid(dx, self.n, self.a, self.m)
+        dx = dx[dx[self.a] == self.m].sort_values('count', ascending = True)
+        dx['frac'] = dx['count']/nc
+        dx = dx[dx['frac'] <= tol]
+        dx = dx_top.merge(dx[[self.n]].drop_duplicates(), on = self.n, how = "inner")
+        dx.sort_values([self.s, self.n], ascending = [True, True], inplace = True)
+        self.d_s_t = dx
+    
+    def create_mat(self, tol, nq):
+        self.sort_n()
+        self.n_best(tol, nq)
+        d_pivot = self.d_n_t.pivot(
+            index = self.s,
+            columns = self.n,
+            values = self.a)
+        self.A = np.array(d_pivot)
+    
+    def save_dat(self, path, tol, nq):
+        # need somehow to test whether this already happened
+        # i.e. can we check whether it exists?
+        self.create_mat(tol, nq)
+        
+        #x = self.d.merge(self.d_n_t, on = "related_q_id", how = "inner")
+        #x = x[["related_q_id", "related_q"]].drop_duplicates()
+        # save stuff
+        #x.to_csv(f"{path}n_20_tol_0_q.csv", index = False)
+        np.savetxt(f"{path}n_20_tol_0.txt", self.A.astype(int), fmt="%i")
+        self.d_n_t.to_csv(f"{path}n_20_tol_0.csv", index = False)
+
+# read data
+df = pd.read_csv("data/raw/df_raw.csv")
+##### BASIC PREPROCESSING #####
+## subset relevant columns
+df = df[["related_q", "related_q_id", "answers", "answer_val", "related_parent_q", "entry_name", "entry_id"]]
+## only overall questions (without parent)
+df = df[df["related_parent_q"].isna()]
+## if more than one answer to a question sample only 1 answer. 
+## maybe this should be later actually 
+df = df.sample(frac = 1.0, random_state=142).groupby(['related_q_id', 'entry_id']).head(1)
+## check for yes/no questions 
+conditions = [
+    (df['answers'] == "Yes"),
+    (df["answers"] == "No"),
+    (df['answers'] == "Field doesn't know") | (df["answers"] == "I don't know") | (df['answers'] == "NaN")
+]
+choices = ["Yes", "No", "DK-NAN"] # how they have mostly coded it 
+df['answer_types'] = np.select(conditions, choices, default="non-binary")
+
+## find questions with non-binary answers and remove them
+df_ = df.groupby(['related_q_id', 'answer_types']).size().reset_index(name="count")
+df_ = df_[df_["answer_types"] == "non-binary"]
+#df_cp = df_
+#df_cp
+#len(df_) # 54704 -- but this goes wrong somehow...?
+df_x = df.merge(df_, how = "outer", indicator = True)
+#len(df_x[df_x['_merge'] == 'left_only']) #50705
+#df_left = df_x[df_x['_merge'] == 'left_only']
+#df_left.groupby('related_q_id').size() # 156
+#len(df_) # 54704
+df_b = df_x[(df_x._merge=="left_only")].drop("_merge", axis = 1)
+#x = df[df["related_q_id"] == 4698]
+#x.groupby('answer_types').size() # it is wrong
+
+#len(df_b) # 50704
+## fill with nan 
+df_nan = fill_grid(df_b, "related_q_id", "entry_id", "DK-NAN")
+#df_b.groupby('answer_types').size()
+## recode values
+answer_map = {
+    "DK-NAN": 0,
+    "Yes": 1,
+    "No": -1
+}
+df_nan = df_nan.assign(answers = df_nan["answer_types"].map(answer_map))
+#df_nan.groupby('answer_types').size() # ...
+df_nan = df_nan[["entry_id", "related_q_id", "answers"]]
+#len(df_nan) # 130416 
+#df_nan[df_nan["related_q_id"] == 4698] # just all 0, so why does it make a difference?
+#df_nan.head(5)
+#df_nan.groupby('answers').size() 
+# -1: 22664 -- a few more  
+#  0: 85735 -- many more here
+#  1: 22017 --- a few more
+len(df_nan)
+x = civs(df_nan, n = 'related_q_id', s = 'entry_id', a = 'answers', m = 0)
+x.create_mat(tol=0, nq=20)
+x.A.shape # (180, 20)--now also (178, 20)??
+ydnt = x.d_n_t
+ydnt
+## why is it not the same as this????
+
+
+### the alternative pipeline ###
+df = pd.read_csv("data/raw/df_raw.csv")
+##### BASIC PREPROCESSING #####
+## subset relevant columns
+df = df[["related_q", "related_q_id", "answers", "answer_val", "related_parent_q", "entry_name", "entry_id"]]
+## only overall questions (without parent)
+df = df[df["related_parent_q"].isna()]
+## if more than one answer to a question sample only 1 answer. 
+## maybe this should be later actually 
+df = df.sample(frac = 1.0, random_state = 142).groupby(['related_q_id', 'entry_id']).head(1)
+
+## check for yes/no questions 
+conditions = [
+    (df['answers'] == "Yes"),
+    (df["answers"] == "No"),
+    (df['answers'] == "Field doesn't know") | (df["answers"] == "I don't know") | (df['answers'] == "NaN")
+]
+choices = [1, -1, 0] # how they have mostly coded it 
+df['answers'] = np.select(conditions, choices, default=100)
+
+## find questions with non-binary answers and remove them
+d_ = df[df["answers"] == 100][["related_q_id"]].drop_duplicates()
+
+#len(d_) # 22 
+#d_.sort_values('related_q_id')
+#df_cp.sort_values('related_q_id')
+#d_
+dfx = df.merge(d_, on = "related_q_id", how = "outer", indicator = True)
+#len(dfx) # 54704
+dfy = dfx[(dfx._merge=="left_only")].drop("_merge", axis = 1) # here we differ
+dfy
+#len(dfy) # 49951 - and there it is
+#len(dfy[dfy['_merge'] == 'left_only']) # 49951
+#len(df_x[df_x['_merge'] == 'left_only']) # 50705
+#df_x_left = df_x[df_x['_merge'] == 'left_only']
+#dfy_left = dfy[dfy['_merge'] == 'left_only']
+#df_x_ls = df_x_left.groupby('related_q_id').size().reset_index(name = 'count')
+#dfy_lls = dfy_left.groupby('related_q_id').size().reset_index(name = 'count')
+#df_x_ls # more 
+#dfy_lls # fewer
+
+#test = pd.concat([dfy_lls, df_x_ls]).drop_duplicates(keep=False)
+#test
+#d_.sort_values(by = 'related_q_id')
+#dfy_lls[dfy_lls["related_q_id"] == 4698] # new one does not
+#df_x_ls[df_x_ls["related_q_id"] == 4698] # this one has it
+#df_x_left[df_x_left["related_q_id"] == 4698] # yep, is there...
+#dfy_left[dfy_left["related_q_id"] == 4698] # not there
+
+## what are these questions? ##
+#df[df["related_q_id"] == 4699]
+
+## fill with nan 
+dfy.dtypes # answers is int64
+df_nanx = fill_grid(dfy, "related_q_id", "entry_id", 0)
+
+df_nanx.groupby('answers').size()
+df_nanx["answers"] = [int(x) for x in df_nanx["answers"]] 
+# zero becomes different things, omg. 
+# just run the whole new pipeline without class
+len(df_nanx)
+df_nanx = df_nanx[["entry_id", "related_q_id", "answers"]]
+x = civs(df_nanx, n = 'related_q_id', s = 'entry_id', a = 'answers', m = 0)
+x.create_mat(tol=0, nq=20)
+x.A.shape # (180, 20) ---slightly smaller..., how?
+dntx = x.d_n_t
+
+# why are they not the same?
+# now they ARE the same 
+pd.concat([ydnt, dntx]).drop_duplicates(keep=False)
