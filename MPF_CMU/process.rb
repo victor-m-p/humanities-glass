f6adf2c2f81e51180cec77a544b2d13b8db64689
Simon J. DeDeo <simon@aeyrie-2.local>
2022-11-27 11:32:13 -0500

    updated fits

--- MPF_CMU/process.rb
+++ MPF_CMU/process.rb
@@ -9,7 +9,7 @@ set=`ls -alh /Users/simon/Desktop/humanities-glass/data/clean`.split("\n").colle
   [n, na, i]
 }.sort { |i,j| (i[0] <=> j[0]) == 0 ? i[1] <=> j[1] : i[0] <=> j[0] }
 
-[2,3].each { |nn|
+[1,2,3].each { |nn|
 
   Parallel.map(set, :in_process=>8) { |trial|
     n_lines=`wc -l #{preface+trial[-1]}`.split(" ")[0].to_i
@@ -25,7 +25,6 @@ set=`ls -alh /Users/simon/Desktop/humanities-glass/data/clean`.split("\n").colle
     file_out.write("#{n_lines}\n#{n}\n")
     file_out.write(str); file_out.close
   
-      print "Doing ./mpf -l #{preface_new+trial[-1]+".mpf"} 0 #{nn}\n"
       ans=`./mpf -l #{preface_new+trial[-1]+".mpf"} 0 #{nn}`
       ans.split("\n").select { |i| i.include?("params") }
       file_out=File.new(preface_new+trial[-1]+".mpf_params_NN#{nn}", 'w')
@@ -34,5 +33,5 @@ set=`ls -alh /Users/simon/Desktop/humanities-glass/data/clean`.split("\n").colle
   
       print "n=#{trial[0]}; maxNA=#{trial[1]}; NN=#{nn}: #{ans.split("\n").select { |i| i.include?("Clock") }[0]}\n"    
   }
-  
+  `cd /Users/simon/Desktop/humanities-glass ; git add . ; git commit -m "updating files #{Time.now}" ; git push`
 }
