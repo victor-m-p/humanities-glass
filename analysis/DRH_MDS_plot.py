2d46a3692715805440167b29d0d1d97fccfaedb1
victor-m-p <victormoeller@gmail.com>
2022-11-28 18:03:33 -0500

    MDS plot

--- analysis/DRH_MDS_plot.py
+++ analysis/DRH_MDS_plot.py
@@ -1,32 +1,40 @@
+import numpy as np 
+import pandas as pd 
+from sim_fun import bin_states, compute_HammingDistance
+from sklearn.manifold import MDS
+import itertools
+import networkx as nx 
+import matplotlib.pyplot as plt
+import os 
 
+# setup 
+n_nodes, maxna = 20, 10
 seed = 2
 n_cutoff = 500
+outpath = '../fig'
 
-## load stuff
-nJ = int(n_nodes*(n_nodes-1)/2)
-A = np.loadtxt(file, delimiter = ',')
-J = A[:nJ]
-h = A[nJ:]
+# read files 
+p_file = '../data/analysis/p_nrow_660_ncol_21_nuniq_20_suniq_581_maxna_10_NN1_LAMBDA0_453839.txt'
+d_file = '../data/clean/matrix_nrow_660_ncol_21_nuniq_20_suniq_581_maxna_10.txt'
+p = np.loadtxt(p_file)
+datastates_weighted = np.loadtxt(d_file)
+datastates = np.delete(datastates_weighted, n_nodes, 1)
+datastates_uniq = np.unique(datastates, axis = 0)
 
-p = p_dist(h, J) # this was okay actually
-allstates = bin_states(n_nodes) 
-p.size
-allstates.size
+# get all state configurations 
+allstates = bin_states(n_nodes) # takes a minute (do not attempt with n_nodes > 20)
 
-# subset the states we want 
+# subset states above threshold
 val_cutoff = np.sort(p)[::-1][n_cutoff]
 p_ind = [i for i,v in enumerate(p) if v > val_cutoff]
 p_vals = p[p > val_cutoff]
 substates = allstates[p_ind]
 perc = round(np.sum(p_vals)*100,2) 
 
-# run it on these substates
-distances = compute_HammingDistance(substates) # here typically euclidean
+# compute hamming distance
+distances = compute_HammingDistance(substates) 
 
-## this takes a while for our data
-## probably because we do not hit epsilon
-## also takes up a HUGE amount of memory
-## probably we include too many possible states
+# set this up when we have a plot we like
 mds = MDS(
     n_components = 2,
     n_init = 4, # number initializations of SMACOF alg. 
@@ -36,36 +44,65 @@ mds = MDS(
     dissimilarity='precomputed',
     n_jobs=-1 # check this if it is slow 
 )
-pos = mds.fit(distances).embedding_ # understand fit, fit_transform
+pos = mds.fit(distances).embedding_ # check fit, fit_transform
 
-# networkx is not super smart so we need to do some stuff here
-states = [x for x in range(len(p_ind))]
-comb = list(itertools.combinations(states, 2))
+# couple configurations, data states and ids
+substates_id = [(num, ele) for num, ele in enumerate(substates)]
+substates_id = pd.DataFrame(substates_id, columns = ['index', 'config']) # this is good
+comb = list(itertools.combinations(substates_id['index'].values, 2))
+
+# find the configurations that are observed in our data
+state_overlap = np.array([x for x in set(tuple(x) for x in substates) & set(tuple(x) for x in datastates_uniq)])
+state_overlap = pd.DataFrame(
+    [(num, ele) for num, ele in enumerate(state_overlap)],
+    columns = ['id_overlap', 'config']
+)
+
+# notably, only 79 ...
+# perhaps we should keep track of the data states 
+# that do not overlap. 
+# interesting that some (unobserved) religions 
+# are more plausible than actually observed religions
+state_overlap_str = state_overlap.astype({'config': str})
+substates_id_str = substates_id.astype({'config': str})
+node_attr = substates_id_str.merge(state_overlap_str, on = 'config', how = 'left', indicator = True)
+node_attr = node_attr.rename(columns = {'_merge': 'datastate'})
+node_attr.replace(
+    {'datastate': 
+        {'both': 'Yes', 'left_only': 'No'}},
+    inplace = True)
+node_attr = node_attr[['index', 'datastate']]
+node_attr['label'] = p_ind
+node_attr['size'] = p_vals
 
 # create network
 G = nx.Graph(comb)
 
-# add node attributes
-d_nodes = pd.DataFrame(
-    list(zip(p_ind, p_vals)),
-    columns = ['label', 'size']
-)
-
-dct_nodes = d_nodes.to_dict('index')
+dct_nodes = node_attr.to_dict('index')
 
 labeldict = {}
 for key, val in dct_nodes.items():
+    G.nodes[key]['index'] = val['index']
     G.nodes[key]['size'] = val['size']
-    labeldict[key] = val['label']
+    G.nodes[key]['datastate'] = val['datastate']
+    labeldict[key] = val['label'] # should change
+
+# create node list for subplot
+ids = list(nx.get_node_attributes(G, 'index').values())
+vals = list(nx.get_node_attributes(G, 'datastate').values())
+nodelst = [id if val == "Yes" else "" for id, val in zip(ids, vals)]
+nodelst = list(filter(None, nodelst))
 
 # prepare plot
 node_scaling = 2000
 size_lst = list(nx.get_node_attributes(G, 'size').values())
 size_lst = [x*node_scaling for x in size_lst]
 
-# plot 
-out = os.path.join(outpath, f'type_{filetype}_nnodes_{n_nodes}_maxna_{maxna}_ncutoff_{n_cutoff}_perc_{perc}_seed_{seed}.jpeg')
-fig, axis = plt.subplots(facecolor = 'w', edgecolor = 'k')
+# make plot 
+out = os.path.join(outpath, f'MDS_nnodes_{n_nodes}_maxna_{maxna}_ncutoff_{n_cutoff}_perc_{perc}_seed_{seed}.pdf')
+fig, axis = plt.subplots(facecolor = 'w', edgecolor = 'k', dpi = 300)
 plt.axis('off')
 nx.draw_networkx_nodes(G, pos, node_size = size_lst)
+nx.draw_networkx_nodes(G, pos, nodelist = nodelst, node_size = 15,
+                       node_color = 'black', node_shape = 'x')
 plt.savefig(f'{out}')
--- analysis/DRH_MDS_prep.py
+++ analysis/DRH_MDS_prep.py
@@ -1,14 +1,5 @@
 import numpy as np 
-import pandas as pd 
-from scipy.spatial.distance import hamming
-from sim_fun import p_dist, bin_states, compute_HammingDistance
-import matplotlib.pyplot as plt 
-from sklearn.decomposition import PCA
-import networkx as nx
-import itertools
-from sklearn.manifold import MDS
-import os 
-pd.set_option('display.max_colwidth', None)
+from sim_fun import p_dist
 
 # setup
 infile = '../data/analysis/matrix_nrow_660_ncol_21_nuniq_20_suniq_581_maxna_10.txt.mpf_params_NN1_LAMBDA0.453839'
@@ -24,67 +15,3 @@ p = p_dist(h, J) # this takes some time (and should not be attempted with n_node
 
 # save this 
 np.savetxt(outname, p)
-
-## this can wait for the next document
-allstates = bin_states(n_nodes) # this takes some time (and should not attempted with n_nodes > 20)
-
-# save these files 
-p.shape
-allstates.shape
-
-# subset the states we want 
-val_cutoff = np.sort(p)[::-1][n_cutoff]
-p_ind = [i for i,v in enumerate(p) if v > val_cutoff]
-p_vals = p[p > val_cutoff]
-substates = allstates[p_ind]
-perc = round(np.sum(p_vals)*100,2) 
-
-# run it on these substates
-distances = compute_HammingDistance(substates) # here typically euclidean
-
-## this takes a while for our data
-## probably because we do not hit epsilon
-## also takes up a HUGE amount of memory
-## probably we include too many possible states
-mds = MDS(
-    n_components = 2,
-    n_init = 4, # number initializations of SMACOF alg. 
-    max_iter = 300, # set up after testing
-    eps=1e-3, # set up after testing
-    random_state=seed,
-    dissimilarity='precomputed',
-    n_jobs=-1 # check this if it is slow 
-)
-pos = mds.fit(distances).embedding_ # understand fit, fit_transform
-
-# networkx is not super smart so we need to do some stuff here
-states = [x for x in range(len(p_ind))]
-comb = list(itertools.combinations(states, 2))
-
-# create network
-G = nx.Graph(comb)
-
-# add node attributes
-d_nodes = pd.DataFrame(
-    list(zip(p_ind, p_vals)),
-    columns = ['label', 'size']
-)
-
-dct_nodes = d_nodes.to_dict('index')
-
-labeldict = {}
-for key, val in dct_nodes.items():
-    G.nodes[key]['size'] = val['size']
-    labeldict[key] = val['label']
-
-# prepare plot
-node_scaling = 2000
-size_lst = list(nx.get_node_attributes(G, 'size').values())
-size_lst = [x*node_scaling for x in size_lst]
-
-# plot 
-out = os.path.join(outpath, f'type_{filetype}_nnodes_{n_nodes}_maxna_{maxna}_ncutoff_{n_cutoff}_perc_{perc}_seed_{seed}.jpeg')
-fig, axis = plt.subplots(facecolor = 'w', edgecolor = 'k')
-plt.axis('off')
-nx.draw_networkx_nodes(G, pos, node_size = size_lst)
-plt.savefig(f'{out}')
