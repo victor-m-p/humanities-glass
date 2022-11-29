77985036148f9ac6a5c9f41b2f61b75ca52a9cac
Simon J. DeDeo <simon@aeyrie-2.fios-router.home>
2022-11-29 12:28:30 -0500

    update search

--- MPF_CMU/new_cv_scan.rb
+++ MPF_CMU/new_cv_scan.rb
@@ -1,5 +1,14 @@
 #!/usr/bin/ruby
-# sbatch -N 1 -o NEW_CV_SCAN -t 12:00:00 -p RM ./new_cv_scan.rb
+# sbatch -N 1 -o NEW_CV_SCAN -t 12:00:00 -p RM ./new_cv_scan.rb 1 1
+# sbatch -N 1 -o NEW_CV_SCAN -t 12:00:00 -p RM ./new_cv_scan.rb 2 1
+# sbatch -N 1 -o NEW_CV_SCAN -t 12:00:00 -p RM ./new_cv_scan.rb 3 1
+# sbatch -N 1 -o NEW_CV_SCAN -t 12:00:00 -p RM ./new_cv_scan.rb 4 1
+# sbatch -N 1 -o NEW_CV_SCAN -t 12:00:00 -p RM ./new_cv_scan.rb 5 1
+# sbatch -N 1 -o NEW_CV_SCAN -t 12:00:00 -p RM ./new_cv_scan.rb 1 2
+# sbatch -N 1 -o NEW_CV_SCAN -t 12:00:00 -p RM ./new_cv_scan.rb 2 2
+# sbatch -N 1 -o NEW_CV_SCAN -t 12:00:00 -p RM ./new_cv_scan.rb 3 2
+# sbatch -N 1 -o NEW_CV_SCAN -t 12:00:00 -p RM ./new_cv_scan.rb 4 2
+# sbatch -N 1 -o NEW_CV_SCAN -t 12:00:00 -p RM ./new_cv_scan.rb 5 2
 
 list=[]
 start=Time.now
@@ -9,13 +18,13 @@ start=Time.now
     beta=rand()**2
   end
   
-  `./mpf -g test 20 128 #{beta}`
-  ans=`./mpf -o test 1`
+  `./mpf -g test_#{ARGV[0]}_#{ARGV[1]} 20 128 #{beta}`
+  ans=`./mpf -o test_#{ARGV[0]}_#{ARGV[1]} #{ARGV[1]}`
   val=[beta]+eval(ans.split("\n").select { |i| i.include?("val=") }[0].split("=")[-1])
   list << val
   print "#{(Time.now-start)/(pos+1)} per loop.\n"
   print "#{val}\n"
-  file=File.new("cv_full.dat", 'w')
+  file=File.new("cv_full_#{ARGV[0]}_#{ARGV[1]}NN.dat", 'w')
   file.write(Marshal.dump(list))
   file.close  
 }
