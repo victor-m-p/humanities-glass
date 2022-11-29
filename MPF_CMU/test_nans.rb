7c2ff517ac912da1e3581b17439dcd5bca0133e5
Simon J. DeDeo <simon@aeyrie-2.wifi.local.cmu.edu>
2022-11-29 18:54:43 -0500

    new fixes

--- MPF_CMU/test_nans.rb
+++ MPF_CMU/test_nans.rb
@@ -22,9 +22,9 @@ start=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{l
 best=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_256_data.dat_params.dat`.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
 
 cut=1024
-str_na=str.split("\n")[1..(128+1)].join("\n")+"\n"+str.split("\n")[129..(128+cut+1)].collect { |j| 
+str_na=str.split("\n")[1..(128+1)].join("\n")+"\n"+str.split("\n")[130..(129+cut+1)].collect { |j| 
   loc=[]
-  while(loc.length < 3) do
+  while(loc.length < nan) do
     while(loc.include?(pos=rand(n))) do     
     end
     loc << pos    
@@ -37,10 +37,10 @@ str_na=str.split("\n")[1..(128+1)].join("\n")+"\n"+str.split("\n")[129..(128+cut
 }.join("\n");1
 
 [64, 128, 256, 512, 512+256, 1024].each { |cut| #, 512, 512+256, 1024
-  file=File.new("DATA/test_sequence_#{label}_128_#{cut}NA3_data.dat", 'w')
+  file=File.new("DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat", 'w')
   file.write("#{128+cut}\n"+str_na); file.close
-  `./mpf -c DATA/test_sequence_#{label}_128_#{cut}NA3_data.dat 1`  
-  ans=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_128_#{cut}NA3_data.dat_params.dat`.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
+  `./mpf -c DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat 1`  
+  ans=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat_params.dat`.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
   print "#{cut}: #{ans} (vs #{best}, vs #{start})\n"
 }
 
@@ -54,27 +54,18 @@ str.split("\n")[2..(128+1)].each { |i|
 };
 avg.collect! { |i| i/128.0 }
 cut=1024
-str_na=str.split("\n")[1..(128+1)].join("\n")+"\n"+str.split("\n")[129..(128+cut+1)].collect { |j| 
-  loc=[]
-  while(loc.length < nan) do
-    while(loc.include?(pos=rand(n))) do     
-    end
-    loc << pos    
-  end
+str_na_new=str_na.split("\n")[1..(128+1)].join("\n")+"\n"+str_na.split("\n")[130..(129+cut+1)].collect { |j| 
   code=j.dup
-  loc.each { |i|
-    code[i]=avg[i].round.to_s
-  }
-  code
+  Array.new(n) { |i| code[i] == "X"  ? avg[i].round.to_s : code[i] }.join("")+" 1.0"
 }.join("\n");1
+
 [64, 128, 256, 512, 512+256, 1024].each { |cut| #, 512, 512+256, 1024
-  file=File.new("DATA/test_sequence_#{label}_128_#{cut}NA3_data.dat", 'w')
-  file.write("#{128+cut}\n"+str_na); file.close
-  `./mpf -c DATA/test_sequence_#{label}_128_#{cut}NA3_data.dat 1`  
-  ans=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_128_#{cut}NA3_data.dat_params.dat`.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
+  file=File.new("DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat", 'w')
+  file.write("#{128+cut}\n"+str_na_new); file.close
+  `./mpf -c DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat 1`  
+  ans=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat_params.dat`.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
   print "#{cut}: #{ans} (vs #{best}, vs #{start})\n"
 }
-
 
 # sbatch -N 1 -o NAN_TESTS_10_1 -t 2:00:00 -p RM ./test_nans.rb 10 1
 # sbatch -N 1 -o NAN_TESTS_10_2 -t 2:00:00 -p RM ./test_nans.rb 10 2
