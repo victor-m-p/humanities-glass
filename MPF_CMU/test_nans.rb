1046c9768dcb7e67586ed66552f44ca65d484039
Simon J. DeDeo <simon@aeyrie-2.fios-router.home>
2022-12-01 15:24:38 -0500

    long NaN test

--- MPF_CMU/test_nans.rb
+++ MPF_CMU/test_nans.rb
@@ -3,83 +3,90 @@
 n=ARGV[0].to_i
 nan=ARGV[1].to_i
 label=ARGV[2]
-`./mpf -g DATA/test_sequence_#{label} #{n} 2048 0.2`
 
-file=File.new("DATA/test_sequence_#{label}_data.dat", 'r')
-str=file.read; file.close
+10.times { |i|
+  print "Starting new test #{i} at time #{Time.now}...\n"
+  
+  `./mpf -g DATA/test_sequence_#{label} #{n} 2048 0.2`
 
-file=File.new("DATA/test_sequence_#{label}_base_data.dat", 'w')
-str2="128\n"+str.split("\n")[1..-1].join("\n");1
-file.write(str2); file.close
+  file=File.new("DATA/test_sequence_#{label}_data.dat", 'r')
+  str=file.read; file.close
 
-file=File.new("DATA/test_sequence_#{label}_256_data.dat", 'w')
-str2="256\n"+str.split("\n")[1..-1].join("\n");1
-file.write(str2); file.close
+  file=File.new("DATA/test_sequence_#{label}_base_data.dat", 'w')
+  str2="128\n"+str.split("\n")[1..-1].join("\n");1
+  file.write(str2); file.close
 
-file=File.new("DATA/test_sequence_#{label}_512_data.dat", 'w')
-str2="512\n"+str.split("\n")[1..-1].join("\n");1
-file.write(str2); file.close
+  file=File.new("DATA/test_sequence_#{label}_256_data.dat", 'w')
+  str2="256\n"+str.split("\n")[1..-1].join("\n");1
+  file.write(str2); file.close
 
-ans=`./mpf -c DATA/test_sequence_#{label}_base_data.dat 2`
-best_sp=ans.scan(/Best log\_sparsity:[^\n]+\n/)[0].split(":")[-1].to_f
+  file=File.new("DATA/test_sequence_#{label}_512_data.dat", 'w')
+  str2="512\n"+str.split("\n")[1..-1].join("\n");1
+  file.write(str2); file.close
 
-`./mpf -c DATA/test_sequence_#{label}_256_data.dat 2`
-`./mpf -c DATA/test_sequence_#{label}_512_data.dat 2`
-start=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_base_data.dat_params.dat`.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
-best=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_256_data.dat_params.dat`.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
-even_bester=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_512_data.dat_params.dat`.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
+  ans=`./mpf -c DATA/test_sequence_#{label}_base_data.dat 2`
+  best_sp=ans.scan(/Best log\_sparsity:[^\n]+\n/)[0].split(":")[-1].to_f
 
-str_na=str.split("\n")[1..129].join("\n")+"\n"+str.split("\n")[130..-1].collect { |j| 
-  loc=[]
-  while(loc.length < nan) do
-    while(loc.include?(pos=rand(n))) do     
+  `./mpf -c DATA/test_sequence_#{label}_256_data.dat 2`
+  `./mpf -c DATA/test_sequence_#{label}_512_data.dat 2`
+  start=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_base_data.dat_params.dat`.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
+  best=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_256_data.dat_params.dat`.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
+  even_bester=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_512_data.dat_params.dat`.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
+
+  str_na=str.split("\n")[1..129].join("\n")+"\n"+str.split("\n")[130..-1].collect { |j| 
+    loc=[]
+    while(loc.length < nan) do
+      while(loc.include?(pos=rand(n))) do     
+      end
+      loc << pos    
     end
-    loc << pos    
-  end
-  code=j.dup
-  loc.each { |i|
-    code[i]="X"
+    code=j.dup
+    loc.each { |i|
+      code[i]="X"
+    }
+    code
+  }.join("\n");1
+
+  [64, 128, 256, 512, 512+256, 1024].each { |cut| #, 512, 512+256, 1024
+    file=File.new("DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat", 'w')
+    file.write("#{128+cut}\n"+str_na); file.close
+    `./mpf -c DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat 2`
+    begin
+      ans=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat_params.dat`
+      ans=ans.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
+      print "#{cut}: #{ans} (vs #{best} vs #{even_bester} vs #{start})\n"
+    rescue
+      print "Something bad happened at #{cut}\n"    
+    end
   }
-  code
-}.join("\n");1
 
-[64, 128, 256, 512, 512+256, 1024].each { |cut| #, 512, 512+256, 1024
-  file=File.new("DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat", 'w')
-  file.write("#{128+cut}\n"+str_na); file.close
-  `./mpf -c DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat 2`
-  begin
-    ans=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat_params.dat`
-    ans=ans.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
-    print "#{cut}: #{ans} (vs #{best} vs #{even_bester} vs #{start})\n"
-  rescue
-    print "Something bad happened at #{cut}\n"    
-  end
-}
+  print "Now do bad choice...\n"
+  avg=Array.new(n) { 0 }
+  str.split("\n")[2..(128+1)].each { |i|
+    set=i.split(" ")[0].split("")
+    n.times { |k|
+      avg[k] += set[k].to_f
+    }
+  };
+  avg.collect! { |i| i/128.0 }
+  str_na_new=str_na.split("\n")[1..-1].collect { |j| 
+    code=j.dup
+    Array.new(n) { |i| code[i] == "X"  ? avg[i].round.to_s : code[i] }.join("")+" 1.0"
+  }.join("\n");1
 
-print "Now do bad choice...\n"
-avg=Array.new(n) { 0 }
-str.split("\n")[2..(128+1)].each { |i|
-  set=i.split(" ")[0].split("")
-  n.times { |k|
-    avg[k] += set[k].to_f
+  [64, 128, 256, 512, 512+256, 1024].each { |cut| #, 512, 512+256, 1024
+    file=File.new("DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat", 'w')
+    file.write("#{128+cut}\n#{n}\n"+str_na_new); file.close
+    `OMP_NUM_THREADS=128 ./mpf -c DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat 2` 
+    begin
+      ans=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat_params.dat`.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
+      print "#{cut}: #{ans} (vs #{best} vs #{even_bester} vs #{start})\n"
+    rescue
+      print "Something bad happened at #{cut}\n"
+    end
   }
-};
-avg.collect! { |i| i/128.0 }
-str_na_new=str_na.split("\n")[1..-1].collect { |j| 
-  code=j.dup
-  Array.new(n) { |i| code[i] == "X"  ? avg[i].round.to_s : code[i] }.join("")+" 1.0"
-}.join("\n");1
-
-[64, 128, 256, 512, 512+256, 1024].each { |cut| #, 512, 512+256, 1024
-  file=File.new("DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat", 'w')
-  file.write("#{128+cut}\n#{n}\n"+str_na_new); file.close
-  `OMP_NUM_THREADS=128 ./mpf -c DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat 2` 
-  begin
-    ans=`./mpf -k DATA/test_sequence_#{label}_base_data.dat DATA/test_sequence_#{label}_params.dat DATA/test_sequence_#{label}_128_#{cut}NA#{nan}_data.dat_params.dat`.scan(/KL:[^\n]+\n/)[0].split(" ")[-1].to_f
-    print "#{cut}: #{ans} (vs #{best} vs #{even_bester} vs #{start})\n"
-  rescue
-    print "Something bad happened at #{cut}\n"
-  end
+  
+  print "Finished test at #{Time.now}\n"
 }
 
 # 3.times { |label|
