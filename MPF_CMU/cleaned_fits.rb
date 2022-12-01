1020e43931ca21c5828cd819f948876ee87ceedf
Simon J. DeDeo <simon@aeyrie-2.fios-router.home>
2022-12-01 09:12:30 -0500

    cleaning tests (adding new data files)

--- MPF_CMU/cleaned_fits.rb
+++ MPF_CMU/cleaned_fits.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/ruby
 # sbatch -N 1 -o CLEANED_FITS -t 10:00:00 -p RM ./cleaned_fits.rb
 
-`ls ../data/mdl_final/cleaned_*`.split("\n").each { |file|
-  start=Time.now
-  print "Doing #{file}...\n"
-  print `OMP_NUM_THREADS=128 ./mpf -c #{file} 1`  
-  print "#{Time.now-start} seconds\n"
+`ls ../data/mdl_final/all_*`.split("\n").each { |file|
+  if (file.scan(/maxna_[0-9]+/)[0].split("_")[-1].to_i <= 5) then
+    start=Time.now
+    print "Doing #{file}...\n"
+    print `OMP_NUM_THREADS=128 ./mpf -c #{file} 1`  
+    print "#{Time.now-start} seconds\n"
 
-  `cd /jet/home/sdedeo/humanities-glass ; git add . ; git commit -m "cleaned no-text fits (from PSC)" ; git push`
+    `cd /jet/home/sdedeo/humanities-glass ; git add . ; git commit -m "cleaned no-text fits (from PSC)" ; git push`
+  end
 }
+
