bb5a4cb39a28f08c4062319ca6fee17c946f1597
Simon J. DeDeo <simon@aeyrie-2.fios-router.home>
2022-12-01 10:56:59 -0500

    holdout tests

--- MPF_CMU/holdout_fits.rb
+++ MPF_CMU/holdout_fits.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/ruby
-# sbatch -N 1 -o HOLDOUT_FITS -t 01:00:00 -p RM ./holdout_fits.rb
+# sbatch -N 1 -o HOLDOUT_FITS_2 -t 03:00:00 -p RM ./holdout_fits.rb
 
 `ls ../data/mdl_final/holdout_*`.split("\n").each { |file|
-  if (file.scan(/maxna_[0-9]+/)[0].split("_")[-1].to_i <= 5) then
+  if (file.scan(/maxna_[0-9]+/)[0].split("_")[-1].to_i >= 4) then
     start=Time.now
     print "Doing #{file}...\n"
     print `OMP_NUM_THREADS=128 ./mpf -c #{file} 1`  
