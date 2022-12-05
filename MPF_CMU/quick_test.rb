a278b986a08b89ebef07c4cc42be199c100d53e0
Simon J. DeDeo <simon@aeyrie-2.lan>
2022-12-05 12:25:39 -0500

    compiler tests

--- MPF_CMU/quick_test.rb
+++ MPF_CMU/quick_test.rb
@@ -1,26 +1,29 @@
-// mpf -l [filename] [logsparsity] [NN] // load in data, fit
-// mpf -c [filename] [NN] // load in data, fit, using cross-validation to pick best sparsity
-// mpf -g [filename] [n_nodes] [n_obs] [beta] // generate data, save both parameters and data to files
-// mpf -t [filename] [paramfile] [NN] // load in test data, fit, get KL divergence from truth
-// mpf -o [filename_prefix] [NN] // load in data (_data.dat suffix), find best lambda using _params.dat to determine KL
-// mpf -k [filename] [paramfile_truth] [paramfile_inferred] // load data, compare truth to inferred
-// mpf -z [paramfile] [n_nodes]  // print out probabilities of all configurations under paramfile
+#!/usr/bin/ruby
 
-./mpf -g TEST/test 20 512 0.5
+`./mpf -g TEST/test 20 512 0.5`
 
-./mpf -z TEST/test_params.dat 20
+`./mpf -z TEST/test_params.dat 20`
 
-./mpf -c TEST/test_data.dat 1
+start=Time.now
+print "Doing GCC case; #{start}\n"
+`./mpf -c TEST/test_data.dat 1`
+print "Finish at #{Time.now-start}\n"
+`cp TEST/test_data.dat_params.dat TEST/test_data.dat_params_CV_GCC.dat`
 
-./mpf -z TEST/test_data.dat_params.dat 20
+start=Time.now
+print "Doing AMD case; #{start}\n"
+`./mpf_AMD -c TEST/test_data.dat 1`
+print "Finish at #{Time.now-start}\n"
 
-cp TEST/test_data.dat_params.dat_probs.dat TEST/test_data.dat_params.dat_probs_CV.dat
-cp TEST/test_data.dat_params.dat TEST/test_data.dat_params_CV.dat
+`./mpf -z TEST/test_data.dat_params.dat 20`
 
-./mpf -l TEST/test_data.dat -100 1
+`cp TEST/test_data.dat_params.dat_probs.dat TEST/test_data.dat_params.dat_probs_CV.dat`
+`cp TEST/test_data.dat_params.dat TEST/test_data.dat_params_CV.dat`
 
-./mpf -z TEST/test_data.dat_params.dat 20
+`./mpf -l TEST/test_data.dat -100 1`
 
-./mpf -k TEST/test_data.dat TEST/test_params.dat TEST/test_data.dat_params_CV.dat
-./mpf -k TEST/test_data.dat TEST/test_params.dat TEST/test_data.dat_params.dat
+`./mpf -z TEST/test_data.dat_params.dat 20`
+
+`./mpf -k TEST/test_data.dat TEST/test_params.dat TEST/test_data.dat_params_CV.dat`
+`./mpf -k TEST/test_data.dat TEST/test_params.dat TEST/test_data.dat_params.dat`
 
