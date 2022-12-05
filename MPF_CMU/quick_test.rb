323005e834878480ceed5f229d9241b1d7926d6c
Simon J. DeDeo <simon@aeyrie-2.lan>
2022-12-05 16:50:09 -0500

    final figure tests

--- MPF_CMU/quick_test.rb
+++ MPF_CMU/quick_test.rb
@@ -4,9 +4,14 @@
 list=[]
 [128, 256, 512].each { |n_data|
   ans=Array.new(10) { |i|
+    if (i.modulo(2) == 0) then
+      beta=0.5
+    else
+      beta=0.25
+    end
     start=Time.now
     
-    `./mpf -g TEST_COMP/test_#{i}_#{n_data}DATA 20 #{n_data} 0.5`
+    `./mpf -g TEST_COMP/test_#{i}_#{n_data}DATA 20 #{n_data} #{beta}`
 
     `./mpf -z TEST_COMP/test_#{i}_#{n_data}DATA_params.dat 20`
 
@@ -24,6 +29,6 @@ list=[]
     cv_performance=`./mpf -k TEST_COMP/test_#{i}_#{n_data}DATA_data.dat TEST_COMP/test_#{i}_#{n_data}DATA_params.dat TEST_COMP/test_#{i}_#{n_data}DATA_data.dat_params_CV.dat`.split("\n")[0].split(":")[-1].to_f
     baseline_performance=`./mpf -k TEST_COMP/test_#{i}_#{n_data}DATA_data.dat TEST_COMP/test_#{i}_#{n_data}DATA_params.dat TEST_COMP/test_#{i}_#{n_data}DATA_data.dat_params.dat`.split("\n")[0].split(":")[-1].to_f
 
-    print "Iteration #{i} #{n_data} (#{Time.now-start} sec.): #{cv_performance} vs #{baseline_performance}\n"
+    print "Iteration #{i} #{n_data} #{beta} (#{Time.now-start} sec.): #{cv_performance} vs #{baseline_performance}\n"
   }  
 }
