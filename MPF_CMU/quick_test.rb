5e68984a46fa088200951e3d43fb648571cf9255
Simon J. DeDeo <simon@aeyrie-2.lan>
2022-12-05 16:47:52 -0500

    final figure data

--- MPF_CMU/quick_test.rb
+++ MPF_CMU/quick_test.rb
@@ -4,7 +4,8 @@
 list=[]
 [128, 256, 512].each { |n_data|
   ans=Array.new(10) { |i|
-
+    start=Time.now
+    
     `./mpf -g TEST_COMP/test_#{i}_#{n_data}DATA 20 #{n_data} 0.5`
 
     `./mpf -z TEST_COMP/test_#{i}_#{n_data}DATA_params.dat 20`
@@ -23,6 +24,6 @@ list=[]
     cv_performance=`./mpf -k TEST_COMP/test_#{i}_#{n_data}DATA_data.dat TEST_COMP/test_#{i}_#{n_data}DATA_params.dat TEST_COMP/test_#{i}_#{n_data}DATA_data.dat_params_CV.dat`.split("\n")[0].split(":")[-1].to_f
     baseline_performance=`./mpf -k TEST_COMP/test_#{i}_#{n_data}DATA_data.dat TEST_COMP/test_#{i}_#{n_data}DATA_params.dat TEST_COMP/test_#{i}_#{n_data}DATA_data.dat_params.dat`.split("\n")[0].split(":")[-1].to_f
 
-    print "Iteration #{i} #{n_data}: #{cv_performance} vs #{baseline_performance}\n"
+    print "Iteration #{i} #{n_data} (#{Time.now-start} sec.): #{cv_performance} vs #{baseline_performance}\n"
   }  
 }
