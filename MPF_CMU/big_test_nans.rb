7bef68e0d96848d991a9c5ffcb2a3d611aed6c01
Simon J. DeDeo <simon@aeyrie-2.lan>
2022-12-06 14:43:07 -0500

    fixing sims

--- MPF_CMU/big_test_nans.rb
+++ MPF_CMU/big_test_nans.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/ruby
 
-# [5].each { |nan|
-#   if (nan >= 4) then
-#     max=40
-#   else
-#     max=1
-#   end
-#   max.times { |k|
-#     print "sbatch -N 1 -o big_NAN_scan_20_#{nan}_#{k} -t 15:00:00 -p RM ./big_test_nans.rb 20 #{nan}\n"
-#   }
-# }
+[5].each { |nan|
+  if (nan >= 4) then
+    max=40
+  else
+    max=1
+  end
+  max.times { |k|
+    print "sbatch -N 1 -o big_NAN_scan_20_#{nan}_#{k+40} -t 15:00:00 -p RM ./big_test_nans.rb 20 #{nan}\n"
+  }
+}
 
 n=ARGV[0].to_i
 nan=ARGV[1].to_i
@@ -26,7 +26,7 @@ final_chunk=[]
   
   print "Starting new test #{i} at time #{Time.now} (#{beta})...\n"
   
-  `./mpf -g DATA/test_sequence_#{label} #{n} 2048 0.25`
+  `./mpf -g DATA/test_sequence_#{label} #{n} 2048 #{beta}`
 
   file=File.new("DATA/test_sequence_#{label}_data.dat", 'r')
   str=file.read; file.close
--- MPF_CMU/nan_table.rb
+++ MPF_CMU/nan_table.rb
@@ -1,13 +1,16 @@
 load '../../ENT/ent.rb'
 
-1.upto(10) { |nan|
-  list=[]
-  Dir.glob("NAN_TESTS/*NAN_20_#{nan}_*").collect { |f|
-    file=File.open(f, 'r'); str=file.read; file.close
-    ans=eval(str.scan(/^\[\[[^\n]+\n/)[-1])
-  }.each { |i|
-    list += i
-  }
-
-  print "#{nan} (#{list.length}): #{list.transpose[0].mean} #{list.transpose[1].mean} #{list.transpose[2].mean} #{list.transpose[3].mean}\n"
+list=[]
+Dir.glob("*NAN_scan_20_5_*").collect { |f|
+  file=File.open(f, 'r'); str=file.read; file.close
+  ans=eval(str.scan(/^\[\[[^\n]+\n/)[-1])
+}.each { |i|
+  list += i
 }
+
+range=[[0.01,0.125], [0.125, 0.25], [0.25, 0.5], [0.5, 1.0]]
+range.each { |i|
+  set=list.select { |j| j[0] > i[0] and j[0] < i[1] }.transpose
+  print "#{i} (#{set.transpose.length}) #{Array.new(4) { |k| set[k+1].mean } }\n"
+};1
+  
