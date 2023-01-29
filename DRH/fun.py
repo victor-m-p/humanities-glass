353de3c17c4b036c8451937a43462745b3c69c1f
Victor Poulsen <vmp@pop-os.localdomain>
2023-01-28 21:41:09 -0500

    trying to make dynamic transition probabilities

--- DRH/fun.py
+++ DRH/fun.py
@@ -9,6 +9,7 @@ import pandas as pd
 import networkx as nx 
 import matplotlib.pyplot as plt 
 from matplotlib.colors import rgb2hex
+from tqdm import tqdm 
 
 # taken from coniii enumerate
 def fast_logsumexp(X, coeffs=None):
@@ -167,3 +168,73 @@ def avg_bitstring(allstates, node_attr, question_ids, node_id_list, node_id_col,
     focal_weights = focal_mat.merge(focal_weights, on = config_id_col, how = 'inner')
     focal_weights = focal_weights.groupby(question_id_col).apply(weighted_average, 'value', weight_col).reset_index(name = 'weighted_avg')
     return focal_weights
+
+def transition_probabilities(configurations, configuration_probabilities,
+                             idx_init, samples, sample_config_idx = False): 
+    """transition probabilities for two neighbor questions
+
+    Args:
+        configurations (np.Array): all configurations (2**20)
+        configuration_probabilities (np.Array): all configuration probabilities (2**20)
+        idx_init (int): index (question number) for the first question
+        samples (int): number of samples 
+        sample_config_idx (list): optionally provide a specific list of indices
+    
+    Returns: Pd.Dataframe with transition probabilities
+    
+    NB: The two questions currently must be neighbors in the question list,
+    e.g. have index 14, and 15 in the question list (in which case idx_init = 14). 
+
+    NB: if specifying sample_config_idx note that this corresponds to the index
+    of the row in 'restricted_configurations', not 'configurations'. 
+    
+    """
+    # reproducibility
+    np.random.seed(seed = 1)
+    # configurations ignoring two indices (i.e. 2**18 rather than 2**20)
+    restricted_configurations = np.delete(configurations, [idx_init, idx_init + 1], 1)
+    restricted_configurations = np.unique(restricted_configurations, axis = 0)
+    n_configurations = len(restricted_configurations)
+    if not sample_config_idx: 
+        sample_config_idx = np.random.choice(n_configurations,
+                                             size = samples,
+                                             replace = False)
+    sample_configs = restricted_configurations[[sample_config_idx]]
+    # set up labels 
+    labels = range(4)
+    # loop over the sample and calculate
+    transition_probabilities = []
+    for num, x in tqdm(enumerate(sample_configs)): 
+        # get the configurations 
+        conf_both = np.insert(x, idx_init, [1, 1])
+        conf_none = np.insert(x, idx_init, [-1, -1])
+        conf_first = np.insert(x, idx_init, [1, -1])
+        conf_second = np.insert(x, idx_init, [-1, 1])
+        # get the configuration idx 
+        idx_both = np.where(np.all(configurations == conf_both, axis = 1))[0][0]
+        idx_none = np.where(np.all(configurations == conf_none, axis = 1))[0][0]
+        idx_first = np.where(np.all(configurations == conf_first, axis = 1))[0][0]
+        idx_second = np.where(np.all(configurations == conf_second, axis = 1))[0][0]
+        # get probabilities
+        p_both = configuration_probabilities[idx_both]
+        p_none = configuration_probabilities[idx_none]
+        p_first = configuration_probabilities[idx_first]
+        p_second = configuration_probabilities[idx_second]
+        # gather in list 
+        probabilities = [p_both, p_none, p_first, p_second]
+        # put this together
+        for p_focal, type_focal in zip(probabilities, labels): 
+            if type_focal == labels[0] or type_focal == labels[1]: 
+                p_neighbors = [probabilities[2], probabilities[3]]
+                type_neighbors = [labels[2], labels[3]]
+            else: 
+                p_neighbors = [probabilities[0], probabilities[1]]
+                type_neighbors = [labels[0], labels[1]]
+            for p_neighbor, type_neighbor in zip(p_neighbors, type_neighbors): 
+                flow = p_neighbor / (p_focal + sum(p_neighbors))
+                transition_probabilities.append((num, type_focal, type_neighbor, flow))
+
+    x = [(x, y, z) for _, x, y, z in transition_probabilities]
+    df = pd.DataFrame(x, columns = ['type_from', 'type_to', 'probability'])
+    df = df.groupby(['type_from', 'type_to'])['probability'].mean().reset_index(name = 'probability')
+    return df 
---
+++ DRH/sacrifice_transitions.py
@@ -0,0 +1,239 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+from tqdm import tqdm 
+import pandas as pd 
+import networkx as nx 
+from fun import transition_probabilities
+
+# check the future warning 
+configurations = np.loadtxt('../data/analysis/configurations.txt')
+configuration_probabilities = np.loadtxt('../data/analysis/configuration_probabilities.txt')
+
+# setup 
+labels = ['AS, CS', '~AS, ~CS', 'AS, ~CS', '~AS, CS']
+label_type = [1, 2, 3, 4]
+idx_first = 14 # adult sacrifice  
+n_samples = 100
+df = transition_probabilities(configurations, 
+                              configuration_probabilities,
+                              idx_first,
+                              n_samples)
+
+# recode 
+conversion_dict = {num:label for num, label in enumerate(labels)}
+df['label_from'] = [conversion_dict.get(x) for x in df['type_from']] 
+df['label_to'] = [conversion_dict.get(x) for x in df['type_to']]
+df = df.sort_values(['type_from', 'type_to'],
+                    ascending = [True, True])
+
+# initialize network
+G = nx.from_pandas_edgelist(df,
+                            source = 'label_from',
+                            target = 'label_to',
+                            edge_attr = 'probability',
+                            create_using = nx.DiGraph)
+
+# edge labels
+edge_width = []
+edge_labels = {}
+for x, y, attr in G.edges(data = True): 
+    weight = attr['probability']
+    edge_width.append(weight)
+    edge_labels[(x, y)] = round(weight, 2)
+
+# positions 
+positions = [(1, 2), (1, 0), (0, 1), (2, 1)]
+pos = {key:pos for key, pos in zip(labels, positions)}
+
+# node labels 
+node_labels = {}
+for i in G.nodes(): 
+    node_labels[i] = i
+
+# edge labels (not well implemented in networkx)
+## need the initial positions to make this work 
+x = nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels);
+
+## take those values out automatically ... ?
+fig, ax = plt.subplots(dpi = 300, figsize = (8, 8))
+plt.axis('off')
+nx.draw_networkx_nodes(G, pos, node_size = 4500, linewidths = 2,
+                       edgecolors = 'k',
+                       node_color = 'white')
+nx.draw_networkx_edges(G, pos, width = [x*5 for x in edge_width],
+                       connectionstyle = "arc3,rad=0.2",
+                       node_size = 5500)
+nx.draw_networkx_labels(G, pos, node_labels)
+
+# edge labels, laborious because networkx...
+nudge_list = [(-0.15, 0.15), # (yes/yes, yes/no): 0.23 
+              (-0.05, -0.05), # (yes/yes, no/yes): 0.22
+              (0.05, -0.05), # (yes/no, yes/yes): 0.4
+              (-0.05, -0.05), # (yes/no, no/no): 0.53
+              (0.15, 0.15), # (no/yes, yes/yes): 0.4
+              (-0.15, 0.15), # (no/yes, no/no): 0.53
+              (0.15, 0.15), # (no/no, yes/no): 0.19
+              (0.05, -0.05)] # (no/no, no/yes): 0.16
+num = 0
+for text, nudge in zip(x.values(), nudge_list): 
+    probability = text.get_text()
+    pos_x, pos_y = text.get_position()
+    nudge_x, nudge_y = nudge 
+    if num in [1, 3, 4, 6]: # not 0, 2, 5 (but this one needs to change)
+        rotation = -50
+    else: 
+        rotation = 50 
+    ax.text(x = pos_x + nudge_x,
+            y = pos_y + nudge_y,
+            s = probability,
+            rotation = rotation,
+            fontsize = 20,
+            horizontalalignment = 'center',
+            verticalalignment = 'center',
+            color = 'black')
+    num += 1
+plt.savefig('../fig/dynamics/sacrifice_transitions.pdf')
+
+# specifically spartan 
+## only the maximum likelihood configuration
+spartans = pd.read_csv('../data/analysis/entry_maxlikelihood.csv')
+spartans = spartans[spartans['entry_name'].str.contains('Archaic Spartan')]
+spartan_idx = spartans['config_id'].tolist()
+spartan_conf = configurations[spartan_idx]
+spartan_restricted = np.delete(spartan_conf, [idx_first, idx_first + 1], 1)
+
+# configurations ignoring two indices (i.e. 2**18 rather than 2**20)
+restricted_configurations = np.delete(configurations, [idx_first, idx_first + 1], 1)
+restricted_configurations = np.unique(restricted_configurations, axis = 0)
+
+spartan_idx_restricted = np.where(np.all(restricted_configurations == spartan_restricted, axis = 1))[0][0]
+
+df = transition_probabilities(configurations, 
+                              configuration_probabilities,
+                              idx_first,
+                              n_samples,
+                              [spartan_idx_restricted])
+
+# recode 
+conversion_dict = {num:label for num, label in enumerate(labels)}
+df['label_from'] = [conversion_dict.get(x) for x in df['type_from']] 
+df['label_to'] = [conversion_dict.get(x) for x in df['type_to']]
+df = df.sort_values(['type_from', 'type_to'],
+                    ascending = [True, True])
+
+# initialize network
+G = nx.from_pandas_edgelist(df,
+                            source = 'label_from',
+                            target = 'label_to',
+                            edge_attr = 'probability',
+                            create_using = nx.DiGraph)
+
+# edge labels
+edge_width = []
+edge_labels = {}
+for x, y, attr in G.edges(data = True): 
+    weight = attr['probability']
+    edge_width.append(weight)
+    edge_labels[(x, y)] = round(weight, 2)
+
+# positions 
+positions = [(1, 2), (1, 0), (0, 1), (2, 1)]
+pos = {key:pos for key, pos in zip(labels, positions)}
+
+# node labels 
+node_labels = {}
+for i in G.nodes(): 
+    node_labels[i] = i
+
+# edge labels (not well implemented in networkx)
+## need the initial positions to make this work 
+x = nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels);
+
+## take those values out automatically ... ?
+fig, ax = plt.subplots(dpi = 300, figsize = (8, 8))
+plt.axis('off')
+nx.draw_networkx_nodes(G, pos, node_size = 4500, linewidths = 2,
+                       edgecolors = 'k',
+                       node_color = 'white')
+nx.draw_networkx_edges(G, pos, width = [x*5 for x in edge_width],
+                       connectionstyle = "arc3,rad=0.2",
+                       node_size = 5500)
+nx.draw_networkx_labels(G, pos, node_labels)
+
+# edge labels, laborious because networkx...
+nudge_list = [(-0.15, 0.15), # (yes/yes, yes/no): 0.23 
+              (-0.05, -0.05), # (yes/yes, no/yes): 0.22
+              (0.05, -0.05), # (yes/no, yes/yes): 0.4
+              (-0.05, -0.05), # (yes/no, no/no): 0.53
+              (0.15, 0.15), # (no/yes, yes/yes): 0.4
+              (-0.15, 0.15), # (no/yes, no/no): 0.53
+              (0.15, 0.15), # (no/no, yes/no): 0.19
+              (0.05, -0.05)] # (no/no, no/yes): 0.16
+num = 0
+for text, nudge in zip(x.values(), nudge_list): 
+    probability = text.get_text()
+    pos_x, pos_y = text.get_position()
+    nudge_x, nudge_y = nudge 
+    if num in [1, 3, 4, 6]: # not 0, 2, 5 (but this one needs to change)
+        rotation = -50
+    else: 
+        rotation = 50 
+    ax.text(x = pos_x + nudge_x,
+            y = pos_y + nudge_y,
+            s = probability,
+            rotation = rotation,
+            fontsize = 20,
+            horizontalalignment = 'center',
+            verticalalignment = 'center',
+            color = 'black')
+    num += 1
+plt.savefig('../fig/dynamics/sacrifice_spartan_maxlikelihood.pdf')
+
+'''
+## sanity check ##
+question_reference = pd.read_csv('../data/analysis/question_reference.csv')
+spartan_idx = spartan_idx[0]
+import configuration as cn 
+SpartanObj = cn.Configuration(spartan_idx,
+                              configurations,
+                              configuration_probabilities)
+neighbor_probs = SpartanObj.neighbor_probabilities(configurations,
+                                                   configuration_probabilities,
+                                                   question_reference)
+
+
+
+
+tst = SpartanObj.pid_neighbors(configurations,
+                               configuration_probabilities)
+
+SpartanObj
+tst = SpartanObj.hamming_neighbors()
+SpartanObj.configuration
+'''
+
+'''
+# find monitor, but not punish 
+pd.set_option('display.max_colwidth', None)
+d = pd.read_csv('/home/vmp/humanities-glass/data/analysis/entry_maxlikelihood.csv')
+unique_config_idx = d['config_id'].unique().tolist()
+unique_configs = configurations[[unique_config_idx]]
+
+monitor = np.where(unique_configs[:, 11] == -1) 
+punish = np.where(unique_configs[:, 12] == 1)
+
+obs_cases = np.intersect1d(monitor, punish)
+unique_config_idx = np.array(unique_config_idx)
+cases = unique_config_idx[obs_cases]
+
+d_case = d[d['config_id'].isin(cases)]
+
+# double check one 
+config_id = 362112
+ConfObj = cn.Configuration(config_id,
+                           configurations,
+                           configuration_probabilities)
+
+question_reference = pd.read_csv('../data/analysis/question_reference.csv')
+question_reference['sanity_check'] = ConfObj.configuration
+'''
