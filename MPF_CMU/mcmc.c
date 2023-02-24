fb6c3d1cd52ddc86fa1756e944a1a172bc0e0782
Simon J. DeDeo <simon@aeyrie-2.local>
2023-02-24 16:24:33 -0500

    marginalizing CV choice

--- MPF_CMU/mcmc.c
+++ MPF_CMU/mcmc.c
@@ -218,11 +218,12 @@ double log_l(all *data, unsigned long int config, double *inferred, int n_blanks
 						count++;
 					}
 				}
-				e_loc_running += e_loc;
+				e_loc_running += exp(e_loc);
 
 			}
 		}
-		return e_loc_running/((double)(1 << n_blanks))-log(z_inferred);			
+		return log(e_loc_running)-log(z_inferred);			
+		//		return e_loc_running/((double)(1 << n_blanks))-log(z_inferred);			
 	}
 }
 
