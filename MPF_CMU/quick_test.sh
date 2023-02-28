6523240f995668a620e313f3168413a133bc0b32
Simon DeDeo <sdedeo@br011.ib.bridges2.psc.edu>
2023-02-28 18:11:09 -0500

    new fits!

--- MPF_CMU/quick_test.sh
+++ MPF_CMU/quick_test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./mpf -c ../data/mdl_experiments/matrix_questions_20_maxna_5_nrows_455_entries_407.txt.mpf_ADDED
+./mpf -c ../data/mdl_experiments/matrix_questions_20_maxna_5_nrows_455_entries_407.txt.mpf_HIDDEN
