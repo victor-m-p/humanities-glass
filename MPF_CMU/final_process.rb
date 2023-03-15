cb9b9c0c4a1aa7458bbc76546244877cb36a6b0a
Simon DeDeo <simon.dedeo@gmail.com>
2023-03-15 17:03:07 -0400

    new LASSO priors

--- MPF_CMU/final_process.rb
+++ MPF_CMU/final_process.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 #!/opt/local/bin/ruby
 
-# sbatch -N 1 -o UPDATED_FITS -t 48:00:00 -p RM ./final_process.rb
+# sbatch -N 1 -o UPDATED_FITS_NoNA -t 48:00:00 -p RM ./final_process.rb
 
 prefix="/jet/home/sdedeo/humanities-glass/data/clean/"
 new_prefix="/jet/home/sdedeo/humanities-glass/data/mdl_experiments/"
@@ -15,7 +15,7 @@ new_prefix="/jet/home/sdedeo/humanities-glass/data/mdl_experiments/"
     n=filename.split("_")[2].to_i
     num_na=filename.split("_")[4].to_i
   
-    if (n == 20) and (num_na == 5) then
+    if (n == 20) and (num_na == 0) then
   
       file=File.new(prefix+filename, 'r')
       str=""
