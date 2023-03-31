a8d0aec74d2fbddbefe5622cfea26a724c642187
Simon DeDeo <simon.dedeo@gmail.com>
2023-03-31 12:19:42 -0400

    fixing MPF code

--- MPF_CMU/nsduh.rb
+++ MPF_CMU/nsduh.rb
@@ -18,7 +18,7 @@ ans=[2.0, 1.0].collect { |p_norm|
     n=file.readline.to_i
 
     print "Doing #{filename} at #{Time.now} with #{p_norm} and #{hidden} hidden nodes\n"
-    filename_out=filename+".mpf"+"_p=#{p_norm}_hidden#{hidden}"
+    filename_out=filename+".mpf"+"_p#{p_norm}_hidden#{hidden}"
     
     file=File.new(prefix+filename, 'r')
     file.readline
@@ -32,6 +32,7 @@ ans=[2.0, 1.0].collect { |p_norm|
     file_out.write("#{n_lines}\n#{n+hidden}\n")
     file_out.write(str); file_out.close
 
+    print "executing: OMP_NUM_THREADS=128 ./mpf -c #{new_prefix+filename_out} #{p_norm}\n"
     ans=`OMP_NUM_THREADS=128 ./mpf -c #{new_prefix+filename_out} #{p_norm}`
     print "FINAL VERSION for #{filename_out}:\n #{ans}\n\n"
 
