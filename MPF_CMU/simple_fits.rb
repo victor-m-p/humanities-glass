6444a957314892359b97edd803bcfffda2bb4bb1
Simon DeDeo <simon.dedeo@gmail.com>
2023-06-28 15:05:16 -0400

    new fits

--- MPF_CMU/simple_fits.rb
+++ MPF_CMU/simple_fits.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 #!/opt/local/bin/ruby
+require 'parallel'
 
 # sbatch -N 1 -o JUNE_FITS_0 -t 48:00:00 -p RM ./simple_fits.rb 0
 # sbatch -N 1 -o JUNE_FITS_1 -t 48:00:00 -p RM ./simple_fits.rb 1
@@ -10,21 +11,29 @@ n=ARGV[0].to_i
 
 filename=`ls ../data/religious-landscapes/*`.split("\n")[n]
 
-Array.new(128) { |i|
-  lambda=-1+2*(i+1)/128.0
-  ans=`mpf -l #{filename+"_LAMBDA#{lambda}_PNORM1"} #{lambda} 1.0`
-  print "LAMBDA=#{lambda}\n#{ans}\n"
+Parallel.map(Array.new(128) { |i| i }, :in_process=>128) { |i|
+  lam=-1+2*(i+1)/128.0
+  `cp #{filename} #{filename+"_lam#{lam}_PNORM1"}`
+  ans=`./mpf -l #{filename+"_lam#{lam}_PNORM1"} #{lam} 1.0`
+  `rm #{filename+"_lam#{lam}_PNORM1"}`
+  print "lam=#{lam}\n#{ans}\n"
 }
 
-ans=`mpf -c #{filename+"_LAMBDA_CV_PNORM1"} 1.0`
+`cp #{filename} #{filename+"_lam_CV_PNORM1"}`
+ans=`./mpf -c #{filename+"_lam_CV_PNORM1"} 1.0`
+`rm #{filename+"_lam_CV_PNORM1"}`
 print "CV\n#{ans}\n"
 
-Array.new(128) { |i|
-  lambda=-1+2*(i+1)/128.0
-  `mpf -l #{filename+"_LAMBDA#{lambda}_PNORM2"} #{lambda} 2.0`
-  print "LAMBDA=#{lambda}\n#{ans}\n"
+Parallel.map(Array.new(128) { |i| i }, :in_process=>128) { |i|
+  lam=-1+2*(i+1)/128.0
+  `cp #{filename} #{filename+"_lam#{lam}_PNORM2"}`
+  ans=`./mpf -l #{filename+"_lam#{lam}_PNORM2"} #{lam} 2.0`
+  `rm #{filename+"_lam#{lam}_PNORM2"}`
+  print "lam=#{lam}\n#{ans}\n"
 }
 
-`mpf -c #{filename+"_LAMBDA_CV_PNORM2"} 2.0`
+`cp #{filename} #{filename+"_lam_CV_PNORM2"}`
+`./mpf -c #{filename+"_lam_CV_PNORM2"} 2.0`
+`rm #{filename+"_lam_CV_PNORM1"}`
 print "CV\n#{ans}\n"
 
