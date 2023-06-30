c070fdb31e0adb3cd931cdd20b803eae41bad046
Simon DeDeo <simon.dedeo@gmail.com>
2023-06-30 13:23:36 -0400

    PSC fits

--- MPF_CMU/simple_fits.rb
+++ MPF_CMU/simple_fits.rb
@@ -2,50 +2,24 @@
 #!/opt/local/bin/ruby
 require 'parallel'
 
-# sbatch -N 1 -o JUNE_FITS_0 -t 24:00:00 -p RM ./simple_fits.rb 0
-# sbatch -N 1 -o JUNE_FITS_1 -t 24:00:00 -p RM ./simple_fits.rb 1
-# sbatch -N 1 -o JUNE_FITS_2 -t 24:00:00 -p RM ./simple_fits.rb 2
-# sbatch -N 1 -o JUNE_FITS_3 -t 24:00:00 -p RM ./simple_fits.rb 3
-
-# sbatch -N 1 -o JUNE_FITS_N0 -t 24:00:00 -p RM ./simple_fits.rb 0
-# sbatch -N 1 -o JUNE_FITS_N1 -t 24:00:00 -p RM ./simple_fits.rb 1
-# sbatch -N 1 -o JUNE_FITS_N2 -t 24:00:00 -p RM ./simple_fits.rb 2
-# sbatch -N 1 -o JUNE_FITS_N3 -t 24:00:00 -p RM ./simple_fits.rb 3
-
-# sbatch -N 1 -o JUNE_FITS_NNN0 -t 48:00:00 -p RM ./simple_fits.rb 0
-# sbatch -N 1 -o JUNE_FITS_NNN1 -t 48:00:00 -p RM ./simple_fits.rb 1
-# sbatch -N 1 -o JUNE_FITS_NNN2 -t 48:00:00 -p RM ./simple_fits.rb 2
-# sbatch -N 1 -o JUNE_FITS_NNN3 -t 48:00:00 -p RM ./simple_fits.rb 3
+# sbatch -N 1 -o JUNE_NEW_0 -t 48:00:00 -p RM ./simple_fits.rb 0
+# sbatch -N 1 -o JUNE_NEW_1 -t 48:00:00 -p RM ./simple_fits.rb 1
+# sbatch -N 1 -o JUNE_NEW_2 -t 48:00:00 -p RM ./simple_fits.rb 2
+# sbatch -N 1 -o JUNE_NEW_3 -t 48:00:00 -p RM ./simple_fits.rb 3
+# sbatch -N 1 -o JUNE_NEW_4 -t 48:00:00 -p RM ./simple_fits.rb 4
+# sbatch -N 1 -o JUNE_NEW_5 -t 48:00:00 -p RM ./simple_fits.rb 5
+# sbatch -N 1 -o JUNE_NEW_6 -t 48:00:00 -p RM ./simple_fits.rb 6
+# sbatch -N 1 -o JUNE_NEW_7 -t 48:00:00 -p RM ./simple_fits.rb 7
 
 n=ARGV[0].to_i
 
-filename=`ls RAW_2/*`.split("\n")[n]
+filename=`ls RAW/*`.split("\n")[n]
 
-transform="WORK_3/"+filename.split("/")[-1]
+transform="WORK/"+filename.split("/")[-1]
 
-Parallel.map(Array.new(128) { |i| i }, :in_process=>128) { |i|
-  lam=-1+2*(i+1)/128.0
-  `cp #{filename} #{transform+"_lam#{lam}_PNORM1"}`
-  ans=`./mpf -l #{transform+"_lam#{lam}_PNORM1"} #{lam} 1.0`
-  `rm #{transform+"_lam#{lam}_PNORM1"}`
-  print "lam=#{lam}\n#{ans}\n"
-}
+print "#{filename}\n"
 
-# `cp #{filename} #{transform+"_lam_CV_PNORM1"}`
-# ans=`./mpf -c #{transform+"_lam_CV_PNORM1"} 1.0`
-# `rm #{transform+"_lam_CV_PNORM2"}`
-# print "CV\n#{ans}\n"
-
-Parallel.map(Array.new(128) { |i| i }, :in_process=>128) { |i|
-  lam=-1+2*(i+1)/128.0
-  `cp #{filename} #{transform+"_lam#{lam}_PNORM2"}`
-  ans=`./mpf -l #{transform+"_lam#{lam}_PNORM2"} #{lam} 2.0`
-  `rm #{transform+"_lam#{lam}_PNORM2"}`
-  print "lam=#{lam}\n#{ans}\n"
-}
-
-# `cp #{filename} #{transform+"_lam_CV_PNORM2"}`
-# `./mpf -c #{transform+"_lam_CV_PNORM2"} 2.0`
-# `rm #{transform+"_lam_CV_PNORM2"}`
-# print "CV\n#{ans}\n"
-
+`cp #{filename} #{transform+"_lam_CV_PNORM1"}`
+ans=`./mpf -c #{transform+"_lam_CV_PNORM1"} 1.0`
+`rm #{transform+"_lam_CV_PNORM2"}`
+print "CV\n#{ans}\n"c
